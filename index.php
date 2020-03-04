dc0df95e13b3a707b6d0acf96f065a444edcb139
smgabier <47088924+smgabier@users.noreply.github.com>
2020-03-04 10:31:01 +0200

    create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+	<title>DevOps Technical Tests</title>
+	</head>
+        <center>
+		<body style="background-color:blue">
+			<h1> The DevOps Team</h1>
+			<p>WE DO PRECISION GUESS WORK BASED ON UNRELIABLE</p>
+			<p>DATA PROVIDED BY THOSE OF QUESTIONABLE KNOWLEDGE</p>
+			<table>
+				<tr>
+					<th></th><th>Id</th>
+					<th>Username</th>
+					<th>Password</th>
+				</tr>
+				
+				<?php $conn = mysqli_connect("localhost", "website", "f878d155d49ac", "company"); 
+				if ($conn-> connect_error) {
+				 die("Connection failed:". $conn-> connect_error);
+				}
+
+				$sql = "SELECT id, username, password from login";
+				$result = $conn-> query($sql);
+
+				if ($result-> num_rows > 0) {
+				   while ($row = $result-> fetch_assoc()) {
+				   echo "<td><td>". $row["id"] ."</td><td>". $row["username"] ."</td><td>". $row["password"] ."<td></tr>";
+
+				   }
+				   echo "</table>";
+
+				}
+				else {
+				   echo "0 result";
+
+				}
+				$conn-> close();
+				?>
+			</table>
+
+		</body>
+		</center>
+</html>
